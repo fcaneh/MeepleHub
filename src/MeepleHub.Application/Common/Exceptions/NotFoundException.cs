b94544cc40dea0787f094ba47cea64e9ca14fffc
fcanehan <fcanehan.jobs@gmail.com>
2026-08-10 13:44:35 +0200

    verif gameid userid avant ajout ludotheque user

---
+++ src/MeepleHub.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeepleHub.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string resourceName, object resourceId)
+            :base($"{resourceName} with identifier '{resourceId}' was not found.")
+        {
+        }
+    }
+}
